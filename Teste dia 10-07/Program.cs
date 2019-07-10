b515885d5143da2f01687aa7c96d03a91219fc56
John Doe <john.doe@gmail.com>
2019-07-10 10:23:56 -0300

    Teste concluído

--- Teste dia 10-07/Program.cs
+++ Teste dia 10-07/Program.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace Teste_dia_10_07
 {
     class Program
     {
+        public static object Thread { get; private set; }
+        public static int i { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello Git");
-            Console.WriteLine("hello world");
+            String[] nameList = new string[2] { "Valor 1", "Valor 2" };
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firsrtTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyyMMddhhmmssfff");
+                Console.WriteLine(dateList[i]);
+            }
+            Console.WriteLine((DateTime.Now - firsrtTime).ToString());
+
+            Console.ReadKey();
         }
     }
 }
