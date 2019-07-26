ede4515a7209e59c97ca88fd6de92a3a579a14f2
Breno <breno.bsiqueira@icloud.com>
2019-07-26 15:02:57 -0300

    CalculadoraShow

---
+++ AcessandoDLL/Program.cs
@@ -0,0 +1,25 @@
+﻿using CalculadoraInterestelar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessandoDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+
+            MostrandoCalculadoraInterestelar c = new MostrandoCalculadoraInterestelar();
+            Console.WriteLine($"somar {c.CalculadoraAdicao(20, 55)}");
+            Console.WriteLine($"Divisao {c.CalculadoraDivisao(20, 55)}");
+            Console.WriteLine($"Multiplicacao {c.CalculadoraMultiplicacao(20, 55)}");
+            Console.WriteLine($"Subtracao {c.CalculadoraSubtracao(20, 55)}");
+
+            Console.ReadKey();
+        }
+    }
+}
